ed2649e5f2b089d5abedcc946635c4cac9302849
Washim-Akram <muhammadwashimakram@gmail.com>
2023-06-12 15:58:45 +0600

    Created a POST API for userCollection

--- index.js
+++ index.js
@@ -39,18 +39,18 @@ async function run() {
       res.send(result);
     });
 
-    // app.post('/users', async(req, res) => {
-    //   const user = req.body;
-    //   const query = { email: user.email }
-    //   const existingUser = await userCollection.findOne(query);
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
 
-    //   if (existingUser) {
-    //     return res.send({ message: 'User already exists' })
-    //   }
+      if (existingUser) {
+        return res.send({ message: 'User already exists' })
+      }
 
-    //   const result = await userCollection.insertOne(user);
-    //   res.send(result);
-    // });
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Instructors Related API's
     app.get("/instructors", async(req, res) => {
