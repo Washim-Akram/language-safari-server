de918b656d79918b9bdda1c430cf1a9afdc89525
Washim-Akram <muhammadwashimakram@gmail.com>
2023-06-12 15:53:50 +0600

    Created userCollection and a get API for it

--- index.js
+++ index.js
@@ -27,15 +27,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const userCollection = client.db("languageSafari").collection("users");
+
     const instructorCollection = client.db("languageSafari").collection("instructors");
 
     const classCollection = client.db("languageSafari").collection("classes");
 
+     // Users Related API's
+     app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.post('/users', async(req, res) => {
+    //   const user = req.body;
+    //   const query = { email: user.email }
+    //   const existingUser = await userCollection.findOne(query);
+
+    //   if (existingUser) {
+    //     return res.send({ message: 'User already exists' })
+    //   }
+
+    //   const result = await userCollection.insertOne(user);
+    //   res.send(result);
+    // });
+
+    // Instructors Related API's
     app.get("/instructors", async(req, res) => {
       const result = await instructorCollection.find().toArray();
       res.send(result);
     });
 
+    // Classes Related API's
     app.get("/classes", async(req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
